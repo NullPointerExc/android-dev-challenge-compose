503b283fef8f9f4d3ae6ded8d9bf489ca06aee8a
Robert <robert.bergmann@mopius.com>
2021-03-07 18:20:27 +0100

    add countdown timer

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,14 +16,30 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.IconButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +55,108 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+    var running by remember { mutableStateOf(false) }
+    var value by remember { mutableStateOf(10L) }
+    var progress by remember { mutableStateOf(1f) }
+    val countDownTimer by remember {
+        mutableStateOf(object : CountDownTimer(11000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                value--
+                progress -= 0.1f
+            }
+
+            override fun onFinish() {
+                value = 10
+                progress = 1f
+                running = false
+            }
+        })
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(color = Color.DarkGray),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box {
+            CircularProgressIndicator(
+                progress = progress, modifier = Modifier
+                    .size(264.dp)
+                    .offset(y = (-16).dp),
+                color = Color.Yellow
+            )
+            Column(
+                modifier = Modifier.size(264.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(modifier = Modifier) {
+                    Surface(
+                        modifier = Modifier.size(75.dp),
+                        shape = CircleShape,
+                        color = Color.Gray
+                    ) {
+                        IconButton(onClick = {
+                            if (!running) {
+                                countDownTimer.start()
+                                running = true
+                            }
+                        }) {
+                            Image(
+                                painterResource(R.drawable.ic_play),
+                                contentDescription = null,
+                                modifier = Modifier.requiredSize(48.dp)
+                            )
+                        }
+                    }
+                    Spacer(Modifier.size(20.dp))
+                    Surface(
+                        modifier = Modifier.size(75.dp),
+                        shape = CircleShape,
+                        color = Color.Gray
+                    ) {
+                        IconButton(onClick = {
+                            countDownTimer.cancel()
+                            value = 10
+                            progress = 1f
+                            running = false
+                        }) {
+                            Image(
+                                painterResource(R.drawable.ic_stop),
+                                contentDescription = null,
+                                modifier = Modifier.requiredSize(48.dp)
+                            )
+                        }
+                    }
+                }
+                Surface(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .offset(y = (-16).dp),
+                    shape = CircleShape,
+                    color = Color.Gray
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(
+                            text = "$value",
+                            color = Color.White,
+                            textAlign = TextAlign.Center,
+                            fontSize = 48.sp,
+                            style = TextStyle(fontWeight = FontWeight.Bold)
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
